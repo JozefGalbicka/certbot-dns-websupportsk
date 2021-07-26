a64f3ca43ebced925482ac2e620c216bf2dc94c0
Jozef Galbička <galb.jozef@gmail.com>
2021-07-26 14:56:06 +0200

    Setup files update

--- setup.py
+++ setup.py
@@ -11,12 +11,39 @@ install_requires = [
     'zope.interface',
 ]
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
 setup(
     name="certbot-dns-websupportsk",
     version=version,
     description="Websupport DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JozefGalbicka/certbot-dns-websupportsk",
     author="Jozef Galbicka",
     author_email="alerts.cryp@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
